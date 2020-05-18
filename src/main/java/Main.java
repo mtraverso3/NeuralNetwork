4c87d68d7e046e7690197f6ff1306afb5cc3a41e
Marcos Traverso <marcostraverso2003@gmail.com>
2020-05-17 19:03:35 -0700

    Added Adding of gradients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.ejml.simple.SimpleMatrix;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,28 +10,47 @@ public class Main
     public static void main(String[] args)
     {
         Network network = new Network(2, List.of(3), 1);
-
-//        double[] input = new double[5];
-//
-//        double[] output = network.evaluate(input);
-//
-//        System.out.println(asList(output));
-//
-//        List<double[]> activations = network.computeActivations(input);
-//
-//        for (double[] activation : activations) {
-//            System.out.println(asList(activation));
-//        }
-
-        dumpGradients(network, new double[] {0, 0}, new double[] {0});
-        dumpGradients(network, new double[] {0, 1}, new double[] {0});
-        dumpGradients(network, new double[] {1, 0}, new double[] {0});
-        dumpGradients(network, new double[] {1, 1}, new double[] {1});
     }
 
-    private static void dumpGradients(Network network, double[] input, double[] ouput)
+    private static void train(Network network, double[][] inputData, double[][] outputData, int amount)
     {
-        List<Layer> layers = network.computeGradients(input, ouput);
+
+        List<Layer> prevGradients = network.computeGradients(inputData[0], outputData[0]);
+        for (int i = 1; i < inputData.length; i++) {
+            List<Layer> gradients = network.computeGradients(inputData[i], outputData[i]);
+            gradients = addLayerLists(gradients, prevGradients);
+            prevGradients = gradients;
+        }
+        scaleLayerList(prevGradients, 1d / inputData.length);
+    }
+
+    private static List<Layer> scaleLayerList(List<Layer> layer1, double scale)
+    {
+        List<Layer> OutputLayer = new ArrayList<Layer>(layer1.size());
+
+        for (int i = 0; i < layer1.size(); i++) {
+            SimpleMatrix weights = layer1.get(i).getWeights().scale(scale);
+            SimpleMatrix biases = layer1.get(i).getBiases().scale(scale);
+            OutputLayer.set(i, new Layer(layer1.get(i).getNeurons(), weights, biases));
+        }
+        return OutputLayer;
+    }
+
+    private static List<Layer> addLayerLists(List<Layer> layer1, List<Layer> layer2)
+    {
+        List<Layer> OutputLayer = new ArrayList<Layer>(layer1.size());
+
+        for (int i = 0; i < layer1.size(); i++) {
+            SimpleMatrix weights = layer1.get(i).getWeights().plus(layer2.get(i).getWeights());
+            SimpleMatrix biases = layer1.get(i).getBiases().plus(layer2.get(i).getBiases());
+            OutputLayer.set(i, new Layer(layer1.get(i).getNeurons(), weights, biases));
+        }
+        return OutputLayer;
+    }
+
+    private static void dumpGradients(Network network, double[] input, double[] output)
+    {
+        List<Layer> layers = network.computeGradients(input, output);
         for (int i = 0; i < layers.size(); i++) {
             System.out.println("=== Layer " + i);
 
